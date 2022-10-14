c7821891e7df801bacbc62663628b019d6a17b10
Elad Armoni <eladarmoni96@gmail.com>
2022-10-14 11:09:58 +0300

    install dotenv

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Exercise = require('./models/Exercise');
-require('dotenv').config(); 
+const dotenv=require('dotenv');
+dotenv.config(); 
 
 const dbUrl=process.env.DB_URL||'mongodb://localhost:27017/gym-exercises';
 mongoose.connect(dbUrl , { useNewUrlParser: true, })
