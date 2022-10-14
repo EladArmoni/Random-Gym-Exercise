9bc0c640197ffe24cdf0f75c6c53e2b682fb7e9f
Elad Armoni <eladarmoni96@gmail.com>
2022-10-14 10:35:32 +0300

    update mongoose connection and node version

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Exercise = require('./models/Exercise');
+require('dotenv').config(); 
 
-mongoose.connect('mongodb://localhost:27017/gym-exercises', { useNewUrlParser: true, })
+const dbUrl=process.env.MONGODB_URI;
+mongoose.connect(dbUrl , { useNewUrlParser: true, })
     .then(() => {
         console.log("Database connected");
     })
     .catch((err) => {
-        console.log("Error while connected to DB!");
+        console.log("Error while connected to DB!",err);
     })
 
 app.set('view engine', 'ejs');
