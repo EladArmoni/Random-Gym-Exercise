9e57c3233b4379275791de25e58256308e97d9fd
Elad Armoni <eladarmoni96@gmail.com>
2022-12-21 13:52:54 +0200

    added index.html for the netlify deployment

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Exercise = require('./models/Exercise');
-const dotenv=require('dotenv');
-dotenv.config(); 
+const dotenv = require('dotenv');
+dotenv.config();
 
 //mongoDB connection
-const dbUrl=process.env.DB_URL;
-mongoose.connect(dbUrl ,{ useNewUrlParser: true})
+const dbUrl = process.env.DB_URL
+mongoose.connect(dbUrl, { useNewUrlParser: true })
     .then(() => {
         console.log("Database connected");
     })
     .catch((err) => {
-        console.log("Error while connected to DB!",err);
+        console.log("Error while connected to DB!", err);
     })
 
 //set engine to ejs
@@ -23,53 +23,53 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const muscles=["Legs","Back","Chest","Shoulders","Biceps","Triceps"];
+const muscles = ["Legs", "Back", "Chest", "Shoulders", "Biceps", "Triceps"];
 
 //home page get route
 app.get('/', (req, res) => {
-    try{
+    try {
         res.render('home');
     }
-    catch{
+    catch {
         res.render('error');
     }
 })
 
 //muscles page get route
 app.get('/muscles', (req, res) => {
-    try{
-        res.render('muscles',{muscles});
+    try {
+        res.render('muscles', { muscles });
     }
-    catch{
+    catch {
         res.render('error');
     }
 })
 
-let button=false;
+let button = false;
 
 //random exercise
 app.get('/type', async (req, res) => {
-    try{
-        button=true;
+    try {
+        button = true;
         const chosenMuscle = req.query.muscle;
         const exercises = await Exercise.find({ "muscle": chosenMuscle });
         const exercise = randomExercise(exercises);
-        res.render('showExercise', { exercise ,button});
+        res.render('showExercise', { exercise, button });
     }
-    catch{
+    catch {
         res.render('error');
     }
 })
 
 //specific exercise
 app.get('/exercise', async (req, res) => {
-    try{
-        button=false;
+    try {
+        button = false;
         const chosenExercise = req.query.exercise;
         const exercise = await Exercise.findOne({ "name": chosenExercise });
-        res.render('showExercise', { exercise,button });
+        res.render('showExercise', { exercise, button });
     }
-    catch{
+    catch {
         res.render('error');
     }
 })
@@ -81,11 +81,11 @@ app.get('/error', async (req, res) => {
 
 //all exercises page get route
 app.get('/showAll', async (req, res) => {
-    try{
+    try {
         const exercises = await Exercise.find({});
         res.render('showAll', { exercises });
     }
-    catch{
+    catch {
         res.render('error');
     }
 })
@@ -96,7 +96,7 @@ function randomExercise(exercises) {
     return exercises[index];
 }
 
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 });
