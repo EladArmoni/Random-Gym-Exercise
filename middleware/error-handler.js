8d487b61e7cd154b890235d91dfea81f3475bf54
Elad Armoni <eladarmoni96@gmail.com>
2023-04-11 16:15:10 +0300

    client - react

--- middleware/error-handler.js
+++
@@ -1,6 +0,0 @@
-const errorHandlerMiddleware = (err, req, res, next) => {
-    console.log(err);
-    res.status(500).json({ msg: err })
-}
-
-module.exports = errorHandlerMiddleware
--- middleware/not-found.js
+++
@@ -1,5 +0,0 @@
-const notFoundMiddleware = (req, res) => {
-    res.status(404).render('error')
-}
-
-module.exports=notFoundMiddleware
