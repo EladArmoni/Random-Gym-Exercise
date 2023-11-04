aeb0302e11234d348da1435ff1ca37452283c522
Elad <eladarmoni96@gmail.com>
2023-11-04 16:56:14 +0200

    remove Navigate

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route,Navigate  } from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import { Home, Muscles, AllExercises, ErrorPage, RandomExercise, Favorites } from './pages'
 import Layout from './components/Layout.js'
 
