6abafd54836b60ec8b146742fcbde0a5e035408e
Elad <eladarmoni96@gmail.com>
2023-11-05 17:37:42 +0200

    change to rander server

--- client/src/JS/url.js
+++ client/src/JS/url.js
@@ -1,5 +1,5 @@
-let local='http://localhost:5000';
-// let server='https://random-exercise.onrender.com';
-let api=local;
+// let local='http://localhost:5000';
+let server='https://random-exercise.onrender.com';
+let api=server;
 
 export default api;
