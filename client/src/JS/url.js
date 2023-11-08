fd51a4d924b9087a106b2d987cc2acbcad721cc3
Elad <eladarmoni96@gmail.com>
2023-11-08 11:19:28 +0200

    server has changed to vercel

--- client/src/JS/url.js
+++ client/src/JS/url.js
@@ -1,7 +1,7 @@
 // let local='http://localhost:5000';
 // let api=local;
 
-let server='https://random-exercise.onrender.com';
+let server='https://random-gym-exercise.vercel.app';
 let api=server;
 
 export default api;
