0fd2d44c1e223f370071a5759ec337bfd2fc2b82
Elad <eladarmoni96@gmail.com>
2023-11-09 16:43:30 +0200

    muscle btns size

--- client/src/components/MuscleBtn.js
+++ client/src/components/MuscleBtn.js
@@ -3,7 +3,7 @@ const MuscleBtn = (props) => {
     return (
         <div className="col-6 col-md-4 mb-1">
             <Link to="/random-exercise" state={{ muscle: props.muscle.name, exercise: null }}>
-                <img className="img-fluid rounded muscle-image" src={props.muscle.image} alt={props.muscle.name} />
+                <img id={props.muscle.name+"_btn"} className="img-fluid rounded muscle-image" src={props.muscle.image} alt={props.muscle.name} />
             </Link>
         </div>
     );
